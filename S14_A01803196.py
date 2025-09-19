22fb0e5875ba522ac2d0c1b91235cbee791c33f2
paolo-cmyk <a01803196@tec.mx>
2025-09-19 08:31:00 -0600

    Add files via upload

---
+++ S14_A01803196.py
@@ -0,0 +1,20 @@
+edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+edad = int(input("Ingresa tu edad: "))
+if edad >= 13 and edad <= 19:
+    print("Eres adolescente")
+else:
+    print("No eres adolescente")
+
+dia = int(input("Ingresa el día de tu nacimiento: "))
+mes = int(input("Ingresa el mes de tu nacimiento (número): "))
+anio = int(input("Ingresa el año de tu nacimiento: "))
+dia_actual = 19
+mes_actual = 9
+anio_actual = 2025
+print("Tienes", edad, "años")
+
